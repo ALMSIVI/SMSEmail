c3738508a7a580bb8d2b9300b4a7f4e1915015c5
ALMSIVI <zakarumbeliever@hotmail.com>
2017-10-21 16:18:52 -0700

    Modified Emailfile

--- src/main/java/util/EmailFile.java
+++ src/main/java/util/EmailFile.java
@@ -1,4 +1,5 @@
 package util;
+import java.util.Arrays;
 
 /**
  * Created by YueWu on 10/21/2017.
@@ -7,29 +8,41 @@ public class EmailFile {
     private String[] recipients;
     private String subject;
     private String message;
-    public static String[] COMMAND = {"SEND", "FORWARD", "REPLY"};
     public static int MIN_INFO = 3;
     public static int MIN_LINE = 2;
 
+    // For send
     public EmailFile(String message) {
         String lines[] = message.split("\n");
         String info[] = lines[0].split(" ");
 
         // TODO: format error
-        if( info.length < MIN_INFO
-                || info[0] != COMAMND[0]
-                || lines.length <= MIN_LINE ) {
-
+        if( info.length < MIN_INFO || lines.length <= MIN_LINE ) {
+            // send text message
         }
 
         else {
-            int subject_index = info.length - 1;
-            this.recipients = Arrays.copyOfRange(info, 1, subject_index - 1);
-            this.subject = info[subject_index];
+            this.recipients = info[1].split(";");
+            this.subject = info[2];
             this.message = message.substring(message.indexOf('\n') + 1);
         }
 
     }
+
+    // For forward and reply
+    public EmailFile(EmailFile original, String newInfo, boolean forward) {
+        if (forward) { // forward
+            this.message = original.getMessage();
+            this.recipients = newInfo.split(";");
+            this.subject = "Fwd: " + original.getSubject();
+        } else { // reply
+            // TODO: On xxx, sb. wrote:
+            this.message = newInfo + "\n------\n" + original.getMessage();
+            this .recipients = original.getRecipients();
+            this.subject = "Re: " + original.getSubject();
+        }
+    }
+
 
     public String[] getRecipients() {
         return recipients;
@@ -40,7 +53,7 @@ public class EmailFile {
     }
 
     public String getMessage() {
-        return passage;
+        return message;
     }
 
 
